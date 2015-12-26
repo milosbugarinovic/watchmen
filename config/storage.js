58c92bed2cb8d43a48de5a1820c139bc40254d23
Milos Bugarinovic <milos@trucktrack.co>
2015-12-26 19:50:44 +0100

    updated libraries

--- config/storage.js
+++ config/storage.js
@@ -1,36 +1,36 @@
 module.exports = {
 
-  production: {
-    provider : 'redis',
-    options : {
-      'redis' : {
-        port: process.env.WATCHMEN_REDIS_PORT_PRODUCTION || 1216,
-        host: '127.0.0.1',
-        db: process.env.WATCHMEN_REDIS_DB_PRODUCTION || 1
-      }
-    }
-  },
+    production: {
+        provider: 'redis',
+        options : {
+            'redis': {
+                port: process.env.WATCHMEN_REDIS_PORT_PRODUCTION || 1216,
+                host: '127.0.0.1',
+                db  : process.env.WATCHMEN_REDIS_DB_PRODUCTION || 1
+            }
+        }
+    },
 
-  development: {
-    provider : 'redis',
-    options : {
-      'redis' : {
-        port: process.env.WATCHMEN_REDIS_PORT_DEVELOPMENT || 1216,
-        host: '127.0.0.1',
-        db: process.env.WATCHMEN_REDIS_DB_DEVELOPMENT || 2
-      }
-    }
-  },
+    development: {
+        provider: 'redis',
+        options : {
+            'redis': {
+                port: process.env.WATCHMEN_REDIS_PORT_DEVELOPMENT || 1216,
+                host: '127.0.0.1',
+                db  : process.env.WATCHMEN_REDIS_DB_DEVELOPMENT || 2
+            }
+        }
+    },
 
-  test: {
-    provider : 'redis',
-    options : {
-      'redis' : {
-        port: process.env.WATCHMEN_REDIS_PORT_TEST || 6666,
-        host: '127.0.0.1',
-        db: process.env.WATCHMEN_REDIS_DB_TEST || 1
-      }
+    test: {
+        provider: 'redis',
+        options : {
+            'redis': {
+                port: process.env.WATCHMEN_REDIS_PORT_TEST || 6666,
+                host: '127.0.0.1',
+                db  : process.env.WATCHMEN_REDIS_DB_TEST || 1
+            }
+        }
     }
-  }
 
 };
--- config/web.js
+++ config/web.js
@@ -1,17 +1,11 @@
 module.exports = {
-
     public_host_name: process.env.WATCHMEN_BASE_URL, // required for OAuth dance
-
     auth: {
-        GOOGLE_CLIENT_ID: process.env.WATCHMEN_GOOGLE_CLIENT_ID || '<Create credentials in Google Dev Console>',
-        GOOGLE_CLIENT_SECRET: process.env.WATCHMEN_GOOGLE_CLIENT_SECRET || '<Create credentials in Google Dev Console>'
+        GOOGLE_CLIENT_ID    : process.env.WATCHMEN_GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.WATCHMEN_GOOGLE_CLIENT_SECRET
     },
-
     port: process.env.WATCHMEN_WEB_PORT, // default port
-
     admins: process.env.WATCHMEN_ADMINS,
-
     ga_analytics_ID: process.env.WATCHMEN_GOOGLE_ANALYTICS_ID,
-
     baseUrl: '/'
 };
